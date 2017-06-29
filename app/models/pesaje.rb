1f8d61ff2369b0c932e28ceb850d499a695bfbeb
oggs22 <adolfoyanes@gmail.com>
2017-06-29 15:15:57 -0400

    refactor

--- app/models/pesaje.rb
+++ app/models/pesaje.rb
@@ -1,10 +1,6 @@
 class Pesaje < ApplicationRecord
 	def self.import(file)
         CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
-        	puts row["parto"]
-        	puts row["parto"].to_date
-        	puts row["pesaje"]
-        	puts row["pesaje"].to_date
         	row["parto"] = row["parto"].to_date
         	row["pesaje"] = row["pesaje"].to_date
           Pesaje.create! row.to_hash
@@ -23,18 +19,11 @@ class Pesaje < ApplicationRecord
          produccion = este_animal.where("lactancia"=>lactancia).order("del ASC")
          if produccion.size > 0 
              base = produccion.first.del * produccion.first.lvd.to_f 
-             puts "base inicial"
-             puts base
              produccion.each_with_index do |f, i| 
-                puts i
                if i > 0
                  promedio = (f.lvd.to_f + produccion[i-1].lvd.to_f )/2 
-                 puts promedio 
                  dias = (f.del - produccion[i-1].del).to_i 
-                 puts dias 
                  base = base + (promedio * dias) 
-                 puts base
-                 puts f.id_2
                end ## cierra if
             end ### cierra do
         else
@@ -48,8 +37,6 @@ class Pesaje < ApplicationRecord
         acumulados = []
         lactancias_totales.each do |f|
             resultado = Pesaje.produccion_acumulada_lactancia(este_animal, f.lactancia).round(2)
-            
-            puts resultado
             if resultado > 0 
                 el_del = este_animal.where("lactancia"=>f.lactancia).order("del DESC").first.del
                 if el_del > 304
