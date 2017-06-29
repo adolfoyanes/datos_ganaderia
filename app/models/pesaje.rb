9b51459af84c07c79d730a8904311160c952fb8d
oggs22 <adolfoyanes@gmail.com>
2017-06-29 10:22:53 -0400

    en funcion

--- app/models/pesaje.rb
+++ app/models/pesaje.rb
@@ -1,13 +1,30 @@
 class Pesaje < ApplicationRecord
 	def self.import(file)
-    CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
-    	puts row["parto"]
-    	puts row["parto"].to_date
-    	puts row["pesaje"]
-    	puts row["pesaje"].to_date
-    	row["parto"] = row["parto"].to_date
-    	row["pesaje"] = row["pesaje"].to_date
-      Pesaje.create! row.to_hash
+        CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
+        	puts row["parto"]
+        	puts row["parto"].to_date
+        	puts row["pesaje"]
+        	puts row["pesaje"].to_date
+        	row["parto"] = row["parto"].to_date
+        	row["pesaje"] = row["pesaje"].to_date
+          Pesaje.create! row.to_hash
+        end
     end
-  end
+
+    def self.produccion_acumulada(este_animal)
+         produccion = este_animal.order("pesaje ASC") 
+         base = produccion.first.del * produccion.first.lvd.to_f 
+         produccion.each_with_index do |f, i| 
+           if i > 1 
+             promedio = (f.lvd.to_f + produccion[i-1].lvd.to_f )/2 
+             puts promedio 
+             dias = (f.pesaje - produccion[i-1].pesaje).to_i 
+             puts dias 
+             base = base + (promedio * dias) 
+             puts base.class
+             puts f.id_2
+           end ## cierra if
+        end ### cierra do
+        return base
+    end
 end
